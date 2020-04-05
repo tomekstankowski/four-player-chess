65e1baebc195aa4317b5790407d06fa31d9adf27
Tomasz Stankowski <stankowski.tomasz1@gmail.com>
2020-04-06 00:15:46 +0200

    removed obsolete dependency

--- engine/build.gradle.kts
+++ engine/build.gradle.kts
@@ -1,4 +1,3 @@
 dependencies {
     implementation("com.github.h0tk3y.betterParse:better-parse-jvm:0.4.0-alpha-3")
-    implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable-jvm:0.3")
 }
